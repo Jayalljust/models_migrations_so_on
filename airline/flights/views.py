1948585322aeac157c37b3175479d75cb345e5f4
Jayalljust <vsevolod.baev@gmail.com>
2020-10-18 14:47:20 +0300

    book form fixed

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -15,7 +15,7 @@ def flight(request, flight_id):
     return render(request, "flights/flight.html", {
         "flight": flight,
         "passengers": flight.passengers.all(),
-        "non_passengers": Passengers.objects.exclude(flights=flight).all()
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 #pk is Primary Key (same as id)
 
